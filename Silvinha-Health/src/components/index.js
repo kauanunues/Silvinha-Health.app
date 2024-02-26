2e07865e39ea329f412370414c8297098f26199c
kauannunes <marykakinho@gmail.com>
2024-02-26 12:27:27 -0300

    criado o projeto e o componente Title

---
+++ Silvinha-Health/src/components/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+export default function Title(){
+    return (
+    <View style={styles.container}>
+        <Textstyle> style={styles.title}Silvinha Health App!</Textstyle>
+        <StatusBar style="auto" />
+      </View>
+    );
+}
